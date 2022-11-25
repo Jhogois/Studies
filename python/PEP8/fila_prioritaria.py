f83118038811613be1d4419b1bd61932646071c5
Jhogois <jhogois@live.com>
2022-11-25 11:28:52 -0300

    Criando Fila base

--- python/PEP8/fila_prioritaria.py
+++ python/PEP8/fila_prioritaria.py
@@ -25,7 +25,7 @@ class FilaPrioriataria(FilaBase):
             estatistica['agencia'] = agencia
             estatistica['clientes_atendidos'] = self.clientes_atendidos
             estatistica['quantidade_clientes_atendidos'] = (
-                    len(self.clientes_atendidos)
-                )
+                len(self.clientes_atendidos)
+            )
 
         return estatistica
